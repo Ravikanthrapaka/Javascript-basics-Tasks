ec1a2a09a99e19c02604afd2b7857bba16dd7d88
Ravikanth <134634315+Ravikanthrapaka@users.noreply.github.com>
2025-03-29 22:24:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,164 @@
+
+
+// let shoppingList = [
+//     "Milk",
+//     "Eggs",
+//     "Bread",
+//     ["Apples", "Bananas", "Oranges"],
+//     ["Potatoes", "Tomatoes", "Onions"]
+// ]
+
+
+// console.log(shoppingList[0])
+
+// console.log(shoppingList)
+
+// // shoppingList.push('Carrots')
+// // console.log(shoppingList)
+
+// // shoppingList.pop()
+// // console.log(shoppingList)
+
+
+// console.log(shoppingList[4])
+// let list=shoppingList[4]
+// list.splice(1,2,"Cucumbers","Bell Peppers")
+// console.log(shoppingList[4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let student = {
+//     name: "Amit",
+//     age: 20,
+//     grade: "A",
+//     address: {
+//         street: "123 Main St",
+//         city: "Anytown",
+//         country: "India"
+//     }
+// };
+
+
+
+// console.log(student.name)
+
+// console.log(student)
+
+// student.phone="123-456-7890"
+// console.log(student)
+
+
+// delete student.grade
+// console.log(student)
+
+// student.age=21
+// console.log(student)
+// console.log(student.age)
+
+
+
+//Conditionals
+
+
+// let num=prompt("Enter a number")
+//     if (num > 0) {
+//         console.log("Positive")
+//     } else if (num < 0) {
+//         console.log("Negative")
+//     } else {
+//         console.log("Zero")
+//     }
+
+
+
+
+
+
+
+
+
+
+
+//Task2
+
+// let marks=55
+// if (marks>=90 && marks<=100){
+
+//     console.log("A")
+// }
+
+// else if(marks>=80 && marks<=89){
+
+//     console.log("B")
+
+
+// }
+
+// else if(marks>=70 && marks<=79){
+
+//     console.log("C")
+
+
+// }
+
+// else if(marks>=60 && marks<=69){
+
+//     console.log("D")
+
+
+// }
+
+// else if(marks<=59){
+
+//     console.log("F")
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Task3
+
+// function checkTruthyFalsy(str) {
+//     if (str) {
+//         return "Truthy";
+//     } else {
+//         return "Falsy";
+//     }
+// }
+
+
+// console.log(checkTruthyFalsy("Hello"));  
+// console.log(checkTruthyFalsy(""));       
+// console.log(checkTruthyFalsy(" "));      
+// console.log(checkTruthyFalsy(null));     
+// console.log(checkTruthyFalsy(undefined));
+// console.log(checkTruthyFalsy(0));
+// console.log(checkTruthyFalsy(-0));
+// console.log(checkTruthyFalsy(10));     
